bc1cb59e6fb43c99928864d0710f09e9eda50856
Cagri <cagrkaraman@gmail.com>
2020-05-31 12:58:56 +0300

    HES data update

---
+++ SRC/Basin_delineation_Duden.py
@@ -0,0 +1,103 @@
+import os, glob
+import whitebox
+import rasterio
+from rasterio.features import shapes as shp
+import geopandas as gpd
+
+wbt = whitebox.WhiteboxTools()
+
+folder = '/mnt/c/Users/cagri/Desktop/Eleman/Duden/Small'
+hes = '/mnt/c/Users/cagri/Desktop/Eleman/Duden/Small/lake_ED50.shp'
+
+
+def raster2polygon(file, path):
+    mask = None
+    with rasterio.open(os.path.join(path, file)) as src:
+        image = src.read(1)  # first band
+        results = (
+            {'properties': {'raster_val': v}, 'geometry': s}
+            for i, (s, v)
+            in enumerate(
+            shp(image, mask=mask, transform=src.transform)))
+    geoms = list(results)
+    boundary = shp(geoms[0]['geometry'])
+    gpd_polygonized_raster = gpd.GeoDataFrame.from_features(geoms)
+    # Filter nodata value
+    gpd_polygonized_raster = gpd_polygonized_raster[gpd_polygonized_raster['raster_val'] > 0]
+    # Convert to geojson
+    gpd_polygonized_raster.crs = 'epsg:23036'
+    name = os.path.basename(path) + '_basins'
+    gpd_polygonized_raster.to_file(
+        driver='ESRI Shapefile', filename=os.path.join(path, name + ".shp"))
+
+
+def rename(folder):
+    name = os.path.basename(folder) + '_basins'
+    try:
+        name = name.replace(" ", "_")
+    except:
+        pass
+    files = glob.glob1(folder, 'basins.*')
+    for file in files:
+        ext = file.split('.')[1]
+        basename = file.split('.')[0]
+        os.rename(os.path.join(folder, file), os.path.join(folder, name + '.' + ext))
+    # code = 'for file in basins.*;do ext=${file##*.};name=$(basename "' + name + '" ".$ext").$ext;mv $file $name; done'
+    # os.system(code)
+
+
+def rename_all(folder):
+    root_dir = os.path.abspath(folder)
+    for item in os.listdir(root_dir):
+        item_full_path = os.path.join(root_dir, item)
+        rename(item_full_path)
+
+
+def delineation(file, folder):
+    wbt.breach_depressions(os.path.join(folder, file), "DEM_breach.tif")
+    wbt.fill_depressions("DEM_breach.tif", "DEM_fill.tif")
+    wbt.flow_accumulation_full_workflow(
+        "DEM_fill.tif", "DEM_out.tif", "Flow_dir.tif", "Flow_acc.tif", log=False)
+    wbt.extract_streams("Flow_acc.tif", "streams.tif", threshold=-1)
+    wbt.snap_pour_points('point.shp', "Flow_acc.tif",
+                         "snap_point.shp", snap_dist=1)
+
+    wbt.watershed("Flow_dir.tif", "snap_point.shp", "Watershed.tif")
+    raster2polygon("Watershed.tif", item_full_path)
+
+
+# Recursively calculate flow direction, accumulation for all basins
+def full_work_flow(folder):
+    try:
+        root_dir = os.path.abspath(folder)
+        for item in os.listdir(root_dir):
+            item_full_path = os.path.join(root_dir, item)
+            wbt.set_working_dir(item_full_path)
+            try:
+                os.remove('dem.tif')
+                os.remove('acc.tif')
+                os.remove('dir.tif')
+            except:
+                pass
+
+            files = glob.glob1(item_full_path, '*.tif')
+            wbt.breach_depressions(os.path.join(item_full_path, files[0]), "DEM_breach.tif")
+            wbt.fill_depressions("DEM_breach.tif", "DEM_fill.tif")
+            wbt.flow_accumulation_full_workflow(
+                "DEM_fill.tif", "DEM_out.tif", "Flow_dir.tif", "Flow_acc.tif", log=False)
+            wbt.extract_streams("Flow_acc.tif", "streams.tif", threshold=-1)
+            basin = glob.glob1(item_full_path, '*.shp')
+            wbt.clip(hes, basin[0], 'at.shp')
+            wbt.snap_pour_points('at.shp', "Flow_acc.tif",
+                                 "snap_point.shp", snap_dist=10000)
+
+            wbt.watershed("Flow_dir.tif", "snap_point.shp", "Watershed.tif")
+            raster2polygon("Watershed.tif", item_full_path)
+    except:
+        pass
+    print(files[0])
+
+
+# full_work_flow(folder)
+# rename_all(folder)
+
