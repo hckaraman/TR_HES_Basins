c452e5b9851d1558475c31a2505a07a8eef0b553
hckaraman <cagrkaraman@gmail.com>
2020-07-03 13:03:34 +0300

    Data extraction added

--- SRC/db_test.py
+++ SRC/db_test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
-engine = create_engine('postgresql://postgres:kalman@localhost:5432/test')
+engine = create_engine('postgresql://postgres:kalman@localhost:5432/clima')
 
 query = """select s."Enlem" ,s."Boylam" ,s."Ortalama_Sıcaklık" from "Sicaklik" s where s."Model" = 'HadGEM2-ES' and s."Senaryo" ='RCP8.5' and s."Ay" =1 and s."Yıl" =2020;"""
 df = pd.read_sql(query, engine)
---
+++ SRC/extract_data.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+from dateutil.relativedelta import relativedelta
+import os, time, glob
+
+start = time.time()
+
+engine = create_engine('postgresql://postgres:kalman@localhost:5432/clima')
+
+date = datetime.datetime(2015, 1, 1)
+
+models = ['MPI-ESM-MR', 'CNRM-CM5']
+
+# models = ['HadGEM2-ES']
+
+senaryos = ['RCP4.5', 'RCP8.5']
+
+query = """select  distinct (bid) from basin b"""
+basins = pd.read_sql(query, engine)
+basins = basins['bid'].values.tolist()
+
+rows_list = []
+
+id = 1
+
+for model in models:
+    for senaryo in senaryos:
+        for basin in basins:
+            # havza = h.strip(".shp")[1:]
+            for y in range(2015, 2090):
+                for m in range(1, 13):
+                    res = []
+
+                    query = f"""select sum(d.discharge) from discharge d where d.yil = {y} and d.ay = {m} and d.model  = '{model}' and d.senaryo  = '{senaryo}' and d.drenajno  in (select hp.hydroid from hru_points hp join basin_points on ST_Contains(basin_points.geom,hp.p_geom) WHERE basin_points.bid  = '{basin}')"""
+                    df = pd.read_sql(query, engine)
+
+                    data = {'id': id, 'Drenaj_No': basin, 'Ay': m, 'Yil': y,
+                            'Discharge': df['sum'].values[0],
+                            'Model': model, 'Senaryo': senaryo}
+                    rows_list.append(data)
+                    print(id, m, y, senaryo, model, basin)
+                    id += 1
+
+df = pd.DataFrame(rows_list)
+df.to_csv(os.path.join('/home/cak/Desktop/TR_HES_Basins/Data/Results', 'Result.csv'))
+
+end = time.time()
+hours, rem = divmod(end - start, 3600)
+minutes, seconds = divmod(rem, 60)
+print("Elapsed Time : {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
